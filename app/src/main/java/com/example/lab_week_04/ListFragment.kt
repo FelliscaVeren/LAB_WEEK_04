06b2670707d99d17abc6c300761be531b5d5e7c2
FelliscaVeren <felvern11@gmail.com>
2025-09-22 11:53:38 +0700

    Commit No. 3 – Tabbeb Navigation

--- app/src/main/java/com/example/lab_week_04/ListFragment.kt
+++ app/src/main/java/com/example/lab_week_04/ListFragment.kt
@@ -1,18 +1,26 @@
 package com.example.lab_week_04
 
 import android.os.Bundle
-import android.view.View
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.navigation.Navigation
 
-class ListFragment : Fragment() {
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ListFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,32 +28,23 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // Ambil referensi tombol coffee
         val coffeeList = listOf<View>(
             view.findViewById(R.id.affogato),
             view.findViewById(R.id.americano),
             view.findViewById(R.id.latte)
         )
-
-        // Set click listener untuk setiap coffee
-        coffeeList.forEach { coffee ->
-            val fragmentBundle = Bundle().apply {
-                putInt(COFFEE_ID, coffee.id)
-            }
-
+        coffeeList.forEach{ coffee ->
+            val fragmentBundle = Bundle()
+            fragmentBundle.putInt(COFFEE_ID, coffee.id)
             coffee.setOnClickListener(
                 Navigation.createNavigateOnClickListener(
-                    R.id.action_listFragment_to_detailFragment2, // ✅ Sesuaikan dengan nav_graph.xml
-                    fragmentBundle
-                )
+                    R.id.action_listFragment_to_detailFragment,
+                    fragmentBundle)
             )
         }
     }
-
     companion object {
         const val COFFEE_ID = "COFFEE_ID"
     }
